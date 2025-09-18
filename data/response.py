5d9b7a46b37e0a2b64dd0d4627006afe5457ba42
User Kondushka <kondushkaml@gmail.com>
2025-09-18 15:07:07 +0300

    Add response.py

---
+++ data/response.py
@@ -0,0 +1,14 @@
+
+class Response:
+
+    MESSAGE_FALSE_USER = 'User already exists'
+    MESSAGE_NO_INGREDIENT = 'Ingredient ids must be provided'
+    MESSAGE_LOST_INPUT = 'Email, password and name are required fields'
+    TOKEN_REFRESH = 'refreshToken'
+    TOKEN_ACCESS = 'accessToken'
+    MESSAGE_LOGIN = 'Bearer'
+    MESSAGE_WRONG_LOGIN = 'email or password are incorrect'
+    MESSAGE_DELETE_USER = "User successfully removed"
+    NO_AUTHORISED = 'You should be authorised'
+    MESSAGE_FALSE_INGREDIENT = "One or more ids provided are incorrect"
+
